782fdd80d9e09db4199cdcc13e642da7db179420
Dishant2112 <dishantgaikwad@gmail.com>
2024-09-12 12:03:21 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+         System.out.println("MAIN CLASS");
+         System.out.println("Time: "+new Date());
+    }
+}
