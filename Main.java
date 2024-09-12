7937e6e639b03c75653dae6b4239e51778ee61ae
Dishant2112 <dishantgaikwad@gmail.com>
2024-09-12 14:41:41 +0530

    Second commit

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Date;
 
 public class Main{
     public static void main(String[] args){
-         System.out.println("MAIN CLASS");
+         System.out.println("MAIN CLASS START");
          System.out.println("Time: "+new Date());
+         System.out.println("MAIN CLASS END")
     }
 }
